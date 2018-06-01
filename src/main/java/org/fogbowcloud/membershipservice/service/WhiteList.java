2f691318b6ee8f1b18afa6c75eee5d1a05ba020b
Francisco Brasileiro <fubica@Fubica.local>
2018-06-01 09:41:02 -0300

    Refactor WhiteList implementation to cache list of members in memory

--- src/main/java/org/fogbowcloud/membershipservice/service/WhiteList.java
+++ src/main/java/org/fogbowcloud/membershipservice/service/WhiteList.java
@@ -2,6 +2,7 @@ package org.fogbowcloud.membershipservice.service;
 
 import org.fogbowcloud.membershipservice.MembershipService;
 
+import org.apache.log4j.Logger;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,41 +12,55 @@ import java.util.Properties;
 
 public class WhiteList implements MembershipService {
 
-    private String membershipConfPath = "membership.conf";
+    private static final Logger LOGGER = Logger.getLogger(WhiteList.class);
+
+    private final String defaultMembershipConfPath = "membership.conf";
+
+    private List<String> membersList;
 
     public WhiteList(String membershipConfPath) {
-        this.membershipConfPath = membershipConfPath;
+        this.membersList = readMembersFromFile(membershipConfPath);
     }
 
-    public WhiteList() {}
+    public WhiteList() {
+        this.membersList = readMembersFromFile(this.defaultMembershipConfPath);
+    }
 
     /**
      * Read list of XMPP members ID from membership config file.
      *
-     * @throws IOException Config file not found or badly-formatted file.
      */
     @Override
-    public List<String> listMembers() throws IOException {
+    public List<String> listMembers() {
+
+        return this.membersList;
+    }
+
+    private List<String> readMembersFromFile(String membershipConfPath) {
         Properties properties = new Properties();
         InputStream input = null;
-        List<String> membersId = new ArrayList<>();
+        List<String> membersList = new ArrayList<>();
 
         try {
-            input = new FileInputStream(this.membershipConfPath);
+            input = new FileInputStream(membershipConfPath);
             properties.load(input);
 
             for (String memberName : properties.stringPropertyNames()) {
                 String memberId = properties.getProperty(memberName);
-                membersId.add(memberId);
+                membersList.add(memberId);
             }
         } catch (IOException e) {
-            throw new IOException("Membership config file was not found.");
+            LOGGER.warn("Error trying to read configuration file found: " + membershipConfPath, e);
         } finally {
             if (input != null) {
-                input.close();
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    LOGGER.warn("Could not close configuration file: " + membershipConfPath, e);
+                }
             }
         }
 
-        return membersId;
+        return membersList;
     }
 }
