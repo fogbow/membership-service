b11396823c763f0114221b927b2af6849dda9f80
fubica <fubica@dsc.ufcg.edu.br>
2018-11-03 18:40:49 -0300

    Rearranged system constants, set log4j level to INFO, fixed version number to comply with new standard

--- src/main/java/org/fogbowcloud/membershipservice/constants/SystemConstants.java
+++ src/main/java/org/fogbowcloud/membershipservice/constants/SystemConstants.java
@@ -1,6 +1,6 @@
 package org.fogbowcloud.membershipservice.constants;
 
 public class SystemConstants {
-    public static final String API_VERSION_NUMBER = "1.1.1";
+    public static final String API_VERSION_NUMBER = "1.0.0";
     public static final String CONF_FILE_NAME = "ms.conf";
 }
