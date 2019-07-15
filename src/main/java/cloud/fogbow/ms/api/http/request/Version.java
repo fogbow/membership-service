dad145f004c6305a3cde3179956520cf6a280417
wassoares <wlfsoares@gmail.com>
2019-07-15 11:19:51 -0300

    Fixed tests of the VersionTest class, with changes made in the Version class

--- src/main/java/cloud/fogbow/ms/api/http/request/Version.java
+++ src/main/java/cloud/fogbow/ms/api/http/request/Version.java
@@ -1,11 +1,10 @@
 package cloud.fogbow.ms.api.http.request;
 
-import cloud.fogbow.common.util.HomeDir;
-import cloud.fogbow.common.constants.ApiDocumentation;
-import cloud.fogbow.ms.api.http.response.VersionNumber;
-import cloud.fogbow.ms.constants.*;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import cloud.fogbow.common.constants.ApiDocumentation;
+import cloud.fogbow.common.util.HomeDir;
+import cloud.fogbow.ms.api.http.response.VersionNumber;
+import cloud.fogbow.ms.constants.ConfigurationPropertyDefaults;
+import cloud.fogbow.ms.constants.ConfigurationPropertyKeys;
+import cloud.fogbow.ms.constants.Messages;
+import cloud.fogbow.ms.constants.SystemConstants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin
 @RestController
@@ -26,47 +29,56 @@ import java.util.Properties;
 @Api(description = ApiDocumentation.Version.API)
 public class Version {
 
-    public static final String VERSION_ENDPOINT = SystemConstants.SERVICE_BASE_ENDPOINT + "version";
+	private static final Logger LOGGER = Logger.getLogger(Version.class);
 
-    private final Logger LOGGER = Logger.getLogger(Version.class);
+	public static final String VERSION_ENDPOINT = SystemConstants.SERVICE_BASE_ENDPOINT + "version";
 
-    @ApiOperation(value = ApiDocumentation.Version.GET_OPERATION)
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<VersionNumber> getVersion() throws FileNotFoundException {
-        String versionNumber = getVersionNumber();
-        return new ResponseEntity<VersionNumber>(new VersionNumber(versionNumber), HttpStatus.OK);
-    }
+	@ApiOperation(value = ApiDocumentation.Version.GET_OPERATION)
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<VersionNumber> getVersion() throws FileNotFoundException {
+		String versionNumber = getVersionNumber();
+		return new ResponseEntity<VersionNumber>(new VersionNumber(versionNumber), HttpStatus.OK);
+	}
 
-    public String getVersionNumber() throws FileNotFoundException {
-        String path = HomeDir.getPath();
-        return getVersionNumber(path + SystemConstants.CONF_FILE_NAME);
-    }
+	public String getVersionNumber() throws FileNotFoundException {
+		String path = HomeDir.getPath();
+		return getVersionNumber(path + SystemConstants.CONF_FILE_NAME);
+	}
 
-    // Used in tests only
-    protected String getVersionNumber(String filePath) throws FileNotFoundException {
-        return SystemConstants.API_VERSION_NUMBER + "-" + readBuildFromFile(filePath);
-    }
+	// Used in tests only
+	protected String getVersionNumber(String filePath) throws FileNotFoundException {
+		return SystemConstants.API_VERSION_NUMBER + "-" + readBuildFromFile(filePath);
+	}
 
-    private String readBuildFromFile(String membershipConfPath) throws FileNotFoundException {
-        Properties properties = new Properties();
-        InputStream input = new FileInputStream(membershipConfPath);
-        String build = "empty";
+	private String readBuildFromFile(String membershipConfPath) throws FileNotFoundException {
+		InputStream input = loadFileInputStream(membershipConfPath);
+		Properties properties = new Properties();
+		String build = "empty";
 
-        try {
-            properties.load(input);
+		try {
+			properties.load(input);
 
-            build = properties.getProperty(ConfigurationPropertyKeys.BUILD_NUMBER_KEY,
-                    ConfigurationPropertyDefaults.BUILD_NUMBER);
-        } catch (IOException e) {
-            LOGGER.warn(String.format(Messages.Warn.ERROR_READING_CONF_FILE, membershipConfPath), e);
-        } finally {
-            try {
-                input.close();
-            } catch (IOException e) {
-                LOGGER.warn(String.format(Messages.Warn.ERROR_CLOSING_CONF_FILE, membershipConfPath), e);
-            }
-        }
+			build = properties.getProperty(ConfigurationPropertyKeys.BUILD_NUMBER_KEY,
+					ConfigurationPropertyDefaults.BUILD_NUMBER);
+		} catch (Exception e) {
+			LOGGER.warn(String.format(Messages.Warn.ERROR_READING_CONF_FILE, membershipConfPath), e);
+		} finally {
+			try {
+				input.close();
+			} catch (Exception e) {
+				LOGGER.warn(String.format(Messages.Warn.ERROR_CLOSING_CONF_FILE, membershipConfPath), e);
+			}
+		}
+		return build;
+	}
 
-        return build;
-    }
+	private InputStream loadFileInputStream(String membershipConfPath) throws FileNotFoundException {
+		InputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(membershipConfPath);
+		} catch (Exception e) {
+			LOGGER.error(String.format(Messages.Error.CONFIGURATION_FILE_NOT_FOUND, membershipConfPath), e);
+		}
+		return inputStream;
+	}
 }
--- src/test/java/cloud/fogbow/ms/api/http/request/VersionTests.java
+++ src/test/java/cloud/fogbow/ms/api/http/request/VersionTests.java
@@ -1,38 +1,79 @@
 package cloud.fogbow.ms.api.http.request;
 
-import cloud.fogbow.common.util.HomeDir;
-import cloud.fogbow.ms.constants.SystemConstants;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.io.FileNotFoundException;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import cloud.fogbow.common.util.HomeDir;
+import cloud.fogbow.ms.constants.SystemConstants;
+
 public class VersionTests {
-    private String VALID_CONF = "ms-test.conf";
-    private String VALID_CONF_WITHOUT_BUILD_PROPERTY = "ms-test-nobuild.conf";
-    private String INVALID_CONF = "invalid.conf";
 
-    @Test
-    public void testVersion() throws Exception {
-        Version version = new Version();
+	private static final String BUILD_PROPERTY = "abcd";
+	private static final String BUILD_PROPERTY_EMPTY = "empty";
+	private static final String INVALID_CONF = "invalid.conf";
+	private static final String VERSION_NUMBER_SEPARATOR = "-";
+	private static final String TESTING_MODE = "[testing mode]";
+	private static final String VALID_CONF = "ms-test.conf";
+	private static final String VALID_CONF_WITHOUT_BUILD_PROPERTY = "ms-test-nobuild.conf";
 
-        String build = version.getVersionNumber(HomeDir.getPath() + this.VALID_CONF);
+	private Version version;
 
-        Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + "-" + "abcd", build);
-    }
+	@Before
+	public void setup() {
+		this.version = new Version();
+	}
 
-    @Test
-    public void testVersionWithoutBuildProperty() throws Exception {
-        Version version = new Version();
+	// test case: When invoking the getVersionNumber method with a parameter
+	// containing a valid file path, it must return the version number concatenated
+	// with the value of the file build number property passed by parameter.
+	@Test
+	public void testVersionWithValidConfPath() throws Exception {
+		// exercise
+		String build = this.version.getVersionNumber(HomeDir.getPath() + VALID_CONF);
 
-        String build = version.getVersionNumber(HomeDir.getPath() + this.VALID_CONF_WITHOUT_BUILD_PROPERTY);
+		// verify
+		Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + BUILD_PROPERTY, build);
+	}
 
-        Assert.assertTrue(build.equals(SystemConstants.API_VERSION_NUMBER + "-" + "[testing mode]"));
-    }
+	// test case: When invoking the getVersionNumber method with a parameter
+	// containing a valid file path but no build number property in this file,
+	// it must return the version of the service in testing mode.
+	@Test
+	public void testVersionWithoutBuildProperty() throws Exception {
+		// exercise
+		String build = this.version.getVersionNumber(HomeDir.getPath() + VALID_CONF_WITHOUT_BUILD_PROPERTY);
 
-    @Test(expected = FileNotFoundException.class)
-    public void testListMembersWithInvalidConfPath() throws FileNotFoundException {
-        Version version = new Version();
-        version.getVersionNumber(this.INVALID_CONF);
-    }
+		// verify
+		Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + TESTING_MODE, build);
+	}
+
+	// test case: When invoking the getVersionNumber method with a parameter
+	// containing an invalid file path, it must return the version number
+	// concatenated with the word empty.
+	@Test
+	public void testVersionWithInvalidConfPath() throws FileNotFoundException {
+		// exercise
+		String build = this.version.getVersionNumber(INVALID_CONF);
+
+		// verify
+		Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + BUILD_PROPERTY_EMPTY,
+				build);
+	}
+
+	// test case: When invoking the getVersionNumber method with no parameters, it
+	// must use a default file and return the version number concatenated the value
+	// of the default file build number property.
+	@Test
+	public void testVersionWithoutConfPath() throws Exception {
+		// exercise
+		String build = this.version.getVersionNumber();
+
+		// verify
+		Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + BUILD_PROPERTY,
+				build);
+	}
+
 }
