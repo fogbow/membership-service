f2c1f369e3977c686a50d3f9d0d5ce0e855f83da
Anderson Dantas <a.kleber.d@gmail.com>
2019-07-19 17:11:54 -0300

    Refactor tests due to changes in code

--- src/test/java/cloud/fogbow/ms/api/http/request/VersionTests.java
+++ src/test/java/cloud/fogbow/ms/api/http/request/VersionTests.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import cloud.fogbow.common.util.HomeDir;
 import cloud.fogbow.ms.constants.SystemConstants;
 
 @RunWith(SpringRunner.class)
@@ -22,12 +21,7 @@ public class VersionTests {
 
     private static final String BASE_ENDPOINT = "/";
     private static final String BUILD_PROPERTY = "abcd";
-    private static final String BUILD_PROPERTY_EMPTY = "empty";
-    private static final String INVALID_CONF = "invalid.conf";
     private static final String VERSION_NUMBER_SEPARATOR = "-";
-    private static final String TESTING_MODE = "[testing mode]";
-    private static final String VALID_CONF = "ms-test.conf";
-    private static final String VALID_CONF_WITHOUT_BUILD_PROPERTY = "ms-test-nobuild.conf";
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,43 +31,6 @@ public class VersionTests {
     @Before
     public void setup() {
         this.version = new Version();
-    }
-
-    // test case: When invoking the getVersionNumber method with a parameter
-    // containing a valid file path, it must return the version number concatenated
-    // with the value of the file build number property passed by parameter.
-    @Test
-    public void testVersionWithValidConfPath() throws Exception {
-        // exercise
-        String build = this.version.getVersionNumber(HomeDir.getPath() + VALID_CONF);
-
-        // verify
-        Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + BUILD_PROPERTY, build);
-    }
-
-    // test case: When invoking the getVersionNumber method with a parameter
-    // containing a valid file path but no build number property in this file,
-    // it must return the version of the service in testing mode.
-    @Test
-    public void testVersionWithoutBuildProperty() throws Exception {
-        // exercise
-        String build = this.version.getVersionNumber(HomeDir.getPath() + VALID_CONF_WITHOUT_BUILD_PROPERTY);
-
-        // verify
-        Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + TESTING_MODE, build);
-    }
-
-    // test case: When invoking the getVersionNumber method with a parameter
-    // containing an invalid file path, it must return the version number
-    // concatenated with the word empty.
-    @Test
-    public void testVersionWithInvalidConfPath() throws FileNotFoundException {
-        // exercise
-        String build = this.version.getVersionNumber(INVALID_CONF);
-
-        // verify
-        Assert.assertEquals(SystemConstants.API_VERSION_NUMBER + VERSION_NUMBER_SEPARATOR + BUILD_PROPERTY_EMPTY,
-                build);
     }
 
     // test case: When invoking the getVersionNumber method with no parameters, it
@@ -104,9 +61,9 @@ public class VersionTests {
     }
 
     private String getJsonResponseContent() {
-        return "{\"version\":" + SystemConstants.API_VERSION_NUMBER 
-                + VERSION_NUMBER_SEPARATOR 
-                + BUILD_PROPERTY 
+        return "{\"version\":" + SystemConstants.API_VERSION_NUMBER
+                + VERSION_NUMBER_SEPARATOR
+                + BUILD_PROPERTY
                 + "}";
     }
 
--- src/test/java/cloud/fogbow/ms/core/service/WhiteListTest.java
+++ src/test/java/cloud/fogbow/ms/core/service/WhiteListTest.java
@@ -1,4 +1,4 @@
-package cloud.fogbow.ms.core.services;
+package cloud.fogbow.ms.core.service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,54 +12,17 @@ public class WhiteListTest {
 
     private MembershipService service;
 
-    private static final String VALID_CONF = "ms-test.conf";
-    private static final String INVALID_CONF = "invalid.conf";
-
     // test case: When invoking the listMembers method from an instance created with
     // the MembershipService class constructor with a valid parameter, it must list
     // the configured membership in the file passed by parameter.
     @Test
-    public void testListMembersWithValidConfPath() throws Exception {
+    public void testListMembers() throws Exception {
         // set up
         List<String> result = new ArrayList<>();
         result.add("xmpp-id1");
         result.add("xmpp-id2");
         result.add("xmpp-id3");
         result.add("xmpp-id4");
-
-        // exercise
-        this.service = new WhiteList(VALID_CONF);
-        List<String> membersId = this.service.listMembers();
-
-        // verify
-        Assert.assertTrue(membersId.contains(result.get(0)));
-        Assert.assertTrue(membersId.contains(result.get(1)));
-        Assert.assertTrue(membersId.contains(result.get(2)));
-        Assert.assertTrue(membersId.contains(result.get(3)));
-    }
-
-    // test case: When invoking the listMembers method from an instance created with
-    // the MembershipService class constructor with an invalid parameter, it must
-    // return an empty list.
-    @Test
-    public void testListMembersWithInvalidConfPath() throws Exception {
-        // exercise
-        this.service = new WhiteList(INVALID_CONF);
-
-        // verify
-        Assert.assertTrue(this.service.listMembers().isEmpty());
-    }
-
-    // test case: When invoking the listMembers method from an instance created with
-    // the default constructor of the MembershipService class with no parameters, it
-    // must list the configured membership in the default file.
-    @Test
-    public void testListMembersWithDefaultConstructor() throws Exception {
-        // set up
-        List<String> result = new ArrayList<>();
-        result.add("member1");
-        result.add("member2");
-        result.add("member3");
 
         // exercise
         this.service = new WhiteList();
@@ -69,6 +32,6 @@ public class WhiteListTest {
         Assert.assertTrue(membersId.contains(result.get(0)));
         Assert.assertTrue(membersId.contains(result.get(1)));
         Assert.assertTrue(membersId.contains(result.get(2)));
+        Assert.assertTrue(membersId.contains(result.get(3)));
     }
-
 }
